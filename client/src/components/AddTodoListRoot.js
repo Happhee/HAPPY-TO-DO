5a0c2ed9f4dc4923119a18ef8fbc606f619bd051
seohee0112 <ghdtj7@gmail.com>
2021-12-20 13:13:00 +0900

    [ADD] 예상 유아이 폰트 추가 및 펜 아이콘 추가 및 박스 스타일 조정

--- client/src/components/AddTodoListRoot.js
+++ client/src/components/AddTodoListRoot.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import AddTodoList from '../containers/AddTodoList';
-
+import '../css/AddTodoListRoot.css'
+import { TiPencil } from "react-icons/ti";
 function AddTodoListRoot() {
     return (
         <div >
-            <h1>서히의 ToDoList</h1>
+            <div className="titleDiv">
+                <span >HAPPY</span>
+                <span >TO</span>
+                <span >DO</span>
+                <TiPencil size="50" textDecoration="underline" />
+            </div>
+            <p className="date">2021 / 01 / 12</p>
             <AddTodoList></AddTodoList>
         </div>
     )
