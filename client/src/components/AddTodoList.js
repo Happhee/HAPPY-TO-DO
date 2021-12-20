f874cdb483c91e5229e57c2d05cb840b8ee7ce98
seohee0112 <ghdtj7@gmail.com>
2021-12-20 13:34:25 +0900

    [ADD] 해야할일 , 남은일 위치 수정 및 유아이 추가

--- client/src/components/AddTodoList.js
+++ client/src/components/AddTodoList.js
@@ -7,20 +7,27 @@ function AddTodoList(props) {
 
     let [todoText, setTodoText] = useState('');
     return (
-        <div>
-            <TextField label="해야 할 일" variant="standard"
-                color="secondary" focused
-                value={todoText} onChange={function (e) {
-                    setTodoText(e.target.value);
-                }} />
-            <IconButton color="secondary" onClick={function () {
-                props.onClick(todoText)
-                setTodoText('');
-            }}>
-                <VscAdd />
-            </IconButton>
-            <div>
-                <span>남은일 </span>{props.remainTodo}<span>개</span>
+        <div className="addTodoList">
+            <p className="date">{props.nowDate}</p>
+            <p className="todoInput">해야 할 일</p>
+
+            <div >
+                <TextField className="todoInput" variant="standard"
+                    color="secondary" focused
+                    value={todoText} onChange={function (e) {
+                        setTodoText(e.target.value);
+                    }} />
+                <IconButton color="secondary" onClick={function () {
+                    props.onClick(todoText)
+                    setTodoText('');
+                }}>
+                    <VscAdd />
+                </IconButton>
+            </div>
+
+            <div className="remainTodo">
+                <span>남은일 </span>
+                <span className="remainTodoProps">{props.remainTodo}</span><span>개</span>
             </div>
         </div>
 
--- client/src/components/AddTodoListRoot.js
+++ client/src/components/AddTodoListRoot.js
@@ -11,7 +11,6 @@ function AddTodoListRoot() {
                 <span >DO</span>
                 <TiPencil size="50" textDecoration="underline" />
             </div>
-            <p className="date">2021 / 01 / 12</p>
             <AddTodoList></AddTodoList>
         </div>
     )
