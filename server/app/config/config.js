9c6a475d82cc0b0f6216755b378bc2c03906e38f
seohee0112 <ghdtj7@naver.com>
2022-01-15 00:04:13 +0900

    feat : 몽고디비 연결 구현
    mongoosedb를 구축하고, 이에대한 url정보를 가져와 서버에서 연동 가능하게 구현
    
    오류 : useNewUrlParser 인식

---
+++ server/app/config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url': 'mongodb+srv://HongSeohee:6496tjgml@cluster0.l0mve.mongodb.net/test'
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
+const config = require('./app/config/config.js');
+const mongoose = require('mongoose');
 const app = express();
 const port = 4000
+
+app.use(bodyParser.json());
+mongoose.connect(config.url, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+    console.log('connected to mongodb server')
+})
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
